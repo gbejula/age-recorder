fe03700eaad8951d4a6bac3461aa8c99d9a5535e
Olugbenga Ajayi <gbejula@gmail.com>
2021-05-14 07:53:18 +0100

    completed add list of users & age

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AgeForm from './components/AgeForm/AgeForm';
+import UsersList from './components/AgeForm/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList(prevUserList => {
+      return [...prevUserList, { name: uName, age: uAge }];
+    });
+  };
+
   return (
     <div>
-      <AgeForm />
+      <AgeForm onAddAge={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
--- src/components/AgeForm/AgeForm.js
+++ src/components/AgeForm/AgeForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,18 +6,39 @@ import Button from '../UI/Button';
 import classes from './AgeForm.module.css';
 
 const AgeForm = props => {
-  const onaddGoalHandler = event => {
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredAge, setEnteredAge] = useState('');
+
+  const addUserHandler = event => {
     event.preventDefault();
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return;
+    }
+    props.onAddAge(enteredName, enteredAge);
+    setEnteredAge('');
+    setEnteredName('');
+  };
+
+  const NameHandler = event => {
+    setEnteredName(event.target.value);
+  };
+
+  const AgeHandler = event => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form>
         <label>Username</label>
-        <input type='text' />
+        <input type='text' value={enteredName} onChange={NameHandler} />
         <label>Age (Years)</label>
-        <input type='text' />
-        <Button type='submit' onClick={onaddGoalHandler}>
+        <input type='text' value={enteredAge} onChange={AgeHandler} />
+        <Button type='submit' onClick={addUserHandler}>
           Add User
         </Button>
       </form>
