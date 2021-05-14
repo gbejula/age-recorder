9e2369d571195d21451c3f2ac2ad63465741e530
Olugbenga Ajayi <gbejula@gmail.com>
2021-05-14 13:35:46 +0100

    completed functionality for error modal

--- src/components/AgeForm/AgeForm.js
+++ src/components/AgeForm/AgeForm.js
@@ -9,14 +9,23 @@ import ErrorModal from '../UI/ErrorModal';
 const AgeForm = props => {
   const [enteredName, setEnteredName] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState('');
 
   const addUserHandler = event => {
     event.preventDefault();
     if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid input (non empty input)',
+      });
       return;
     }
 
     if (+enteredAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (> 0)',
+      });
       return;
     }
     props.onAddAge(enteredName, enteredAge);
@@ -32,9 +41,19 @@ const AgeForm = props => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal title='An error occured!' message='Something went wrong!' />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onDissmiss={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form>
           <label>Username</label>
